c449c9d40d24af7351f966b5402ed70d45eb0598
simolex <simolex@yandex.ru>
2025-04-09 23:29:42 +0300

    save task A

---
+++ Stage-1/task-A/index.js
@@ -0,0 +1,55 @@
+/**
+ * Каждому по компьютеру
+ */
+
+function eachComputers(n, m, students, rooms) {}
+
+const _readline = require("readline");
+
+const _reader = _readline.createInterface({
+    input: process.stdin
+});
+
+const _inputLines = [];
+let _curLine = 0;
+
+_reader.on("line", (line) => {
+    _inputLines.push(line);
+});
+
+process.stdin.on("end", solve);
+
+function solve() {
+    const [n, m] = readArray();
+
+    const students = readArray();
+    const rooms = readArray();
+
+    const result = eachComputers(n, m, students, rooms);
+
+    console.log(result.length);
+    console.log(result.join(" "));
+}
+
+function readInt() {
+    const n = Number(_inputLines[_curLine]);
+    _curLine++;
+    return n;
+}
+
+function readArray() {
+    var arr = _inputLines[_curLine]
+        .trim(" ")
+        .split(" ")
+        .map((num) => Number(num));
+    _curLine++;
+    return arr;
+}
+
+function readString() {
+    const s = _inputLines[_curLine].trim();
+    _curLine++;
+    return s;
+}
+
+module.exports = eachComputers;
