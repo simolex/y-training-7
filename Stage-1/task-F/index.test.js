cab05dadf1a28cdbeef5367f7d10d12da48c0121
simolex <simolex@yandex.ru>
2025-04-10 20:38:13 +0300

    save 1F

---
+++ Stage-1/task-F/index.test.js
@@ -0,0 +1,8 @@
+const solution = require(".");
+
+describe("F. Рюкзак: наибольшая стоимость с восстановлением ответа", () => {
+    test("test-1", () => {
+        const result = solution(4, 6, [2, 4, 1, 2], [7, 2, 5, 1]);
+        expect(result).toEqual([1, 3, 4]);
+    });
+});
