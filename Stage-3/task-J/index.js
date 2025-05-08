e54b154bdcaba83eb24e23381aa01400cc2b42f2
simolex <simolex@yandex.ru>
2025-05-08 10:26:17 +0300

    done task 3J

--- Stage-3/task-J/index.js
+++ Stage-3/task-J/index.js
@@ -2,6 +2,8 @@
  * Каждому по компьютеру
  */
 
+const codeSizes = Array.from({ length: 24 }, (_, i) => [1 << i, i]);
+
 function pack(text) {
     let len = text.length;
     const dictionary = {};
@@ -11,8 +13,11 @@ function pack(text) {
         dictionary[String.fromCharCode(i)] = i - a + 1;
     }
     let nextCode = 27;
+    let codeSizeIndex = 0;
+    let currentCodeSize = 1;
 
     let left = 0;
+    let outputCode = "";
     while (left < len) {
         let right = left;
         let current = text[right++];
@@ -20,41 +25,91 @@ function pack(text) {
             current += text[right++];
         }
 
+        while (codeSizes[codeSizeIndex][0] < nextCode) {
+            currentCodeSize = codeSizes[++codeSizeIndex][1];
+        }
+
         if (!(current in dictionary)) {
-            result.push(dictionary[current.slice(0, -1)], current.slice(-1));
+            outputCode += dictionary[current.slice(0, -1)].toString(2).padStart(currentCodeSize, "0");
+            outputCode += dictionary[current.slice(-1)].toString(2).padStart(5, "0");
+
             dictionary[current] = nextCode++;
         } else {
-            result.push(dictionary[current]);
+            outputCode += dictionary[current].toString(2).padStart(currentCodeSize, "0");
+        }
+
+        while (outputCode.length >= 8) {
+            result.push(parseInt(outputCode.slice(0, 8), 2));
+            outputCode = outputCode.slice(8);
         }
         left = right;
     }
 
-    console.log(result);
-    return [];
+    if (outputCode.length > 0) {
+        result.push(parseInt(outputCode.slice(0, 8).padEnd(8, "0"), 2));
+    }
+
+    return result;
 }
 
 function unpack(n, codes) {
-    return "";
+    let result = "";
+    const dictionary = [""];
+
+    let a = "a".charCodeAt(0);
+    for (let i = a; i <= "z".charCodeAt(0); i++) {
+        dictionary.push(String.fromCharCode(i));
+    }
+    let nextCode = dictionary.length;
+    let codeSizeIndex = 0;
+    let currentCodeSize = 1;
+
+    let prefix = "";
+    let currentCode = "";
+    let currentByte = 0;
+    let inputBuffer = "";
+    while (currentByte < n) {
+        while (codeSizes[codeSizeIndex][0] < nextCode) {
+            currentCodeSize = codeSizes[++codeSizeIndex][1];
+        }
+
+        while (currentByte < n && inputBuffer.length < currentCodeSize + 5) {
+            inputBuffer += codes[currentByte++].toString(2).padStart(8, "0");
+        }
+
+        currentCode = inputBuffer.slice(0, currentCodeSize);
+        inputBuffer = inputBuffer.slice(currentCodeSize);
+        prefix = dictionary[parseInt(currentCode, 2)];
+        result += prefix;
+
+        if (inputBuffer.length >= 5) {
+            currentCode = inputBuffer.slice(0, 5);
+            inputBuffer = inputBuffer.slice(5);
+            prefix += dictionary[parseInt(currentCode, 2)];
+            result += dictionary[parseInt(currentCode, 2)];
+            dictionary.push(prefix);
+            nextCode++;
+        }
+    }
+    return result;
 }
 
 const _readline = require("readline");
 
 const _reader = _readline.createInterface({
-    input: process.stdin,
+    input: process.stdin
 });
 
 const _inputLines = [];
 let _curLine = 0;
 
 let command;
-let code;
 
 _reader.on("line", (line) => {
     _inputLines.push(line);
 
     if (_inputLines.length === 1) {
         command = readString();
-        console.log(command);
     } else if (command === "pack" && _inputLines.length === 2) {
         const result = pack(readString());
         console.log(result.length);
